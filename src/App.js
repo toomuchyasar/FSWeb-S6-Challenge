99f1fb966e8f019ff1a434e40ac54556a1b90641
toomuchyasar <yasarmuslu93@outlook.com>
2023-04-30 13:49:53 +0300

    %15

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import Karakter from './components/Karakter';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      <hr />
+       <Karakter />
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,34 @@
-// Karakter bileşeniniz buraya gelecek
+import axios from "axios"
+import { useEffect , useState } from "react"
+import "../components/Karakter.css"
+
+
+const Karakter = () => {
+
+    const [starWars, setStarWars] = useState([])
+
+    useEffect (() => {
+        axios.get("https://swapi.dev/api/people/")
+        .then((resp)=>{
+            setStarWars(resp.data)
+        })
+    },[])
+
+    const bilgileriGoster = (item) => {
+       return console.log(item.name)
+    }
+
+    return<div>
+        <div className="selectChButton">
+            {starWars.map((ch) => {
+                return <div className="chPage" key={ch.name}>
+                    <h3>{ch.name}</h3>
+                     <button onClick={() =>bilgileriGoster(ch)
+                    }> Bilgileri Göster</button>
+                    </div>   
+            })}
+        </div>
+    </div>
+}
+
+  export default Karakter
